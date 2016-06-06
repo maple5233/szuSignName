1cce383fdf60484502ec5b4bd172fc143ef53f97
maple5233 <326153996@qq.com>
2016-06-06 22:06:20 +0800

    finished menu

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,6 +1,8 @@
 var vm = new Vue({
 	el:'#whole',
 	data: {
+		// 已经点过名
+		hasSigned : false,
 		// 班级信息
 		classList : ['班级1','班级2','班级3','班级4'],
 		classChosen : '班级1',
@@ -11,10 +13,10 @@ var vm = new Vue({
 		chosingInfo : false,
 		checkingInfo : false,
 		// 选中班级的信息
-		timeList : ['时间1','时间2','时间3','时间4'],
+		timeList : ['2014-05-13','2014-05-14','2014-05-15','2014-05-16'],
 		studentList : ['学生1','学生2','学生3','学生4'],
 		// 选择了某个学生
-		studentChosen : '洪继耀',
+		studentChosen : '',
 		checkStudent: false,
 		timeInfo : [
 		{
@@ -39,7 +41,7 @@ var vm = new Vue({
 		}
 		],
 		// 选择了某个时间
-		timeChosen : '2014-05-13',
+		timeChosen : '',
 		checkTime : true, 
 		studentInfo : [
 		{
@@ -67,16 +69,33 @@ var vm = new Vue({
 	methods: {
 		// 选择查看信息
 		selectTime : function (event) {
+			this.timeChosen = event.target.innerHTML;
+			/* 更新studentInfo */
 			this.signing = false
 			this.chosingInfo = false
-			console.log(event)
+			this.checkTime = true
+			this.checkStudent = false
+			this.checkingInfo = true
 		},
-		selectStudnt : function (event) {
+		selectStudent : function (event) {
+			this.studentChosen = event.target.innerHTML;
+			/* 更新timeInfo */
 			this.signing = false
 			this.chosingInfo = false
+			this.checkTime = false
+			this.checkStudent = true
+			this.checkingInfo = true
+		},
+		// 选择信息查看种类
+		chooseInfo : function (event) {
+			/* 根据选择的班级更新classChosen timeList studentList */
+			this.signing = false
+			this.checkingInfo = false
+			this.chosingInfo = true
 		},
 		// 开关点名
 		beginSigning : function (event) {
+			this.hasSigned = true
 			this.signing = true
 			this.chosingInfo = false
 			this.checkInfo = false
@@ -84,12 +103,28 @@ var vm = new Vue({
 			/* 将二维码地址赋值 */
 		},
 		endSigning : function (event) {
+			if (!this.hasSigned) {return}
 			this.signing = false
+			this.chosingInfo = false
 			/* 查看刚刚生成的点名表 */
 			this.checkingInfo = true
 			// 更新timeInfo和studentInfo数组
 			/* 设置时间为当前时间 然后查看当前时间按的签到表 */
 			// this.timeChosen = ...
+		},
+		// 该变签到状态
+		changeSign : function (index,cla,event) {
+			// console.log(event)
+			/* 以下更新视图 */
+			if(cla === 1){
+				// console.log(1)
+				this.timeInfo[index].signed = !this.timeInfo[index].signed
+				this.timeInfo[index].signText = this.timeInfo[index].signed?'已到':'未到'
+			} else {
+				this.studentInfo[index].signed = !this.studentInfo[index].signed
+				this.studentInfo[index].signText = this.studentInfo[index].signed?'已到':'未到'
+			}
+			/* 此处发出请求更新后台数据 */
 		}
 	}
 })
